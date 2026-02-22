e803476fa4011383f72e3c4c8c9c9d049e968fa1
thngvu20244-cmd <thangvu20244@gmail.com>
2026-02-22 08:46:05 +0700

    Update app.py
    .

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Home Page"
-
-@app.route("/login")
-def login():
-    return "Login Page"
+    return render_template("login.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
