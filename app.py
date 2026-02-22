1a749e68f3cd0807f6e807d1ad0d71506d527c07
thngvu20244-cmd <thangvu20244@gmail.com>
2026-02-22 08:15:09 +0700

    Update app.py
    .

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Home Page"
+
 @app.route("/login")
 def login():
     return "Login Page"
+
+if __name__ == "__main__":
+    app.run()
